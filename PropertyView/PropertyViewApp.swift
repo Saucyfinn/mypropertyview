b11aa476d0de383bb3b9f3cb163b7f70a6cb19fd
Brendon Hogg <tasmanlakeshome@gmail.com>
2025-08-28 08:19:50 +1200

    Initial Commit

---
+++ PropertyView/PropertyViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyViewApp.swift
+//  PropertyView
+//
+//  Created by Brendon Hogg on 28/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct PropertyViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
